4de05b2ab7e727405cf703e792cf5d5aa9930910
Breyten Ernsting <bje@dds.nl>
2013-07-22 12:12:24 +0200

    Addresses.

--- base/static/js/main.js
+++ base/static/js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-    $('#map-continents').cssMap({'size' : 960});
+    $('#map-continents').cssMap({
+        agentsListId : '#addresses',
+        // optional: fade in/fade out delay in miliseconds;
+        agentsListSpeed : 500,
+        // optional: show and hide agent/address on hover;
+        agentsListOnHover : true,
+        size : 960
+    });
 });
